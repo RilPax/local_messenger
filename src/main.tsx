85fe6b191945b5fa40bbe9fef9e7e239b31107b9
Нормуминов Руслан <RilPax@yandex.ru>
2026-09-04 15:04:10 +0500

    refactor: organize global styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from '@/Store/store';
 import { App } from '@/App';
-import '@/global.scss';
+import '@/Styles/global.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
